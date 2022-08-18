dc431a00b10b2c69eb2efb397e656b4c205e6fdf
teacherIan <ian_malloy@ssis.asia>
2022-08-18 12:39:13 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//imports
+import * as THREE from 'three';
+//scene initialization
+import { camera, scene, renderer } from './init';
+
+//You can change the camera position on the X, Y, or Z axis
+camera.position.set(0, 0, 10);
+
+const demoBox = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(1, 1, 1),
+  new THREE.MeshNormalMaterial()
+);
+
+scene.add(demoBox);
+
+//Any code to initialize your objects should go here
+
+const loop = () => {
+  //Any code that will need to be updated on each frame should go here
+
+  //update scene on next frame
+  renderer.render(scene, camera);
+  requestAnimationFrame(loop);
+};
+
+loop();
