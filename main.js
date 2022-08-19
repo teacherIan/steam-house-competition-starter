d6c58dd79c27fbf2b3cd4c256ebabfd59cb43b1d
teacherIan <ian_malloy@ssis.asia>
2022-08-19 12:32:44 +0800

    Updated createCube and createSphere functions

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
 //imports
 import * as THREE from 'three';
 //scene initialization
-import { camera, scene, renderer } from './init';
+import {
+  camera,
+  scene,
+  renderer,
+  createSphere,
+  createCube,
+  objectArray,
+} from './init';
 
 //You can change the camera position on the X, Y, or Z axis
 camera.position.set(0, 0, 10);
 
-const demoBox = new THREE.Mesh(
-  new THREE.BoxBufferGeometry(1, 1, 1),
-  new THREE.MeshNormalMaterial()
-);
-
-scene.add(demoBox);
-
 const clock = new THREE.Clock();
 
-//Any code to initialize your objects should go here
+//Any code to initialize your objects should go here.
+//Any object created using createSphere() or createCube()
+//Will be added to an array named 'objectArray'
+
+createSphere();
+createCube();
+createSphere();
+createCube();
+createSphere();
+
+for (let i = 0; i < objectArray.length; i++) {
+  const margin = 3;
+  const mid = (objectArray.length - 1) * 0.5 * margin;
+  objectArray[i].position.x = i * margin - mid;
+  console.log(objectArray[i].position);
+}
 
 const loop = () => {
   //You can use the clock get elapsed time method to get the time since the start of the program
   clock.getElapsedTime();
   //Any code that will need to be updated on each frame should go here
-
-  //have the box rotate each frame:
-  demoBox.rotateX(0.002);
 
   //update scene on next frame
   renderer.render(scene, camera);
